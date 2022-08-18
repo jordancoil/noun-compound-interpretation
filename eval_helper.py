497c7bd09d129e11bca8eb434a20fb175655eda3
Jordan Coil <albert.jordan.coil@gmail.com>
2022-08-18 12:26:24 -0700

    adding batch scoring methods

--- eval_helper.py
+++ eval_helper.py
@@ -1,9 +1,11 @@
 from datasets import load_metric
 
+import semeval_scorer
+
 rouge = load_metric('rouge')
 meteor = load_metric('meteor')
 bleu = load_metric('bleu')
-bertscore = load_metric('bertscore')
+bert = load_metric('bertscore')
 
 
 def rougeL_f1_scorer(candidate, reference):
@@ -22,7 +24,7 @@ def bleu_scorer(candidate, reference):
 
 
 def bert_scorer(candidate, reference, model_type='???'):
-    score = bertscore.compute(predictions=[candidate], references=[reference], model_type=model_type)
+    score = bert.compute(predictions=[candidate], references=[reference], model_type=model_type)
     return score['f1'][0]
 
 
@@ -59,3 +61,97 @@ def batch_meteor(gen_paraphrases, ref_paraphrases):
 
     avg_score = meteor.compute()
     return avg_score['meteor']
+
+
+def batch_meteor_score(gen_paraphrases, ref_paraphrases):
+    running_avg = 0
+    for gen_para in gen_paraphrases:
+        scores = []
+        for ref_para in ref_paraphrases:
+            score = meteor.compute(predictions=[gen_para], references=[ref_para])
+            scores.append(score['meteor'])
+        top_score = max(scores)
+        running_avg += top_score
+
+    avg_score = running_avg / len(gen_paraphrases)
+    return avg_score
+
+
+def batch_rougel_score(gen_paraphrases, ref_paraphrases):
+    for gen_para in gen_paraphrases:
+        for ref_para in ref_paraphrases:
+            rouge.add_batch(predictions=[gen_para], references=[ref_para])
+
+    rouge_l_scores = rouge.compute(use_aggregator=False)['rougeL']
+    rouge_l_f1_scores = [sc.fmeasure for sc in rouge_l_scores]
+
+    running_avg = 0
+    for i in range(len(gen_paraphrases)):
+        start = i * len(gen_paraphrases)
+        end = start + len(gen_paraphrases)
+        f1_max = max(rouge_l_f1_scores[start:end])
+        running_avg += f1_max
+
+    avg_score = running_avg / len(gen_paraphrases)
+    return avg_score
+
+
+def batch_bertsc_score(gen_paraphrases, ref_paraphrases):
+    for gen_para in gen_paraphrases:
+        for ref_para in ref_paraphrases:
+            bert.add_batch(predictions=[gen_para], references=[ref_para])
+    bert_f1_scores = bert.compute(lang='en')['f1']
+
+    running_avg = 0
+    for i in range(len(gen_paraphrases)):
+        start = i * len(gen_paraphrases)
+        end = start + len(gen_paraphrases)
+        f1_max = max(bert_f1_scores[start:end])
+        running_avg += f1_max
+
+    avg_score = running_avg / len(gen_paraphrases)
+    return avg_score
+
+
+def batch_semval_score(gen_paraphrases, ref_paraphrases):
+    avg_score = semeval_scorer.get_paraphrase_score(gen_paraphrases, ref_paraphrases)
+    return avg_score
+
+
+def all_scores(generated_df, reference_df):
+    global_total_meteor = 0
+    global_total_rougel = 0
+    global_total_bertsc = 0
+    global_total_semval = 0
+
+    for index, row in generated_df.iterrows():
+        print("progress: ", index, "/", len(generated_df))
+        target_nc = row['nc']
+        gen_paras = row['paras']
+        print("scoring: '", target_nc, "'")
+
+        ref_paras = reference_df.loc[reference_df['nc'] == target_nc]['paraphrases'].iloc[0]
+
+        meteor_score = batch_meteor_score(gen_paras, ref_paras)
+        rougel_score = batch_rougel_score(gen_paras, ref_paras)
+        bertsc_score = batch_bertsc_score(gen_paras, ref_paras)
+        semval_score = batch_semval_score(gen_paras, ref_paras)
+
+        global_total_meteor += meteor_score
+        global_total_rougel += rougel_score
+        global_total_bertsc += bertsc_score
+        global_total_semval += semval_score
+
+    num_paras = len(generated_df)
+
+    global_average_meteor = global_total_meteor / num_paras
+    global_average_rougel = global_total_rougel / num_paras
+    global_average_bertsc = global_total_bertsc / num_paras
+    global_average_semval = global_total_semval / num_paras
+
+    print("global meteor, ", global_average_meteor)
+    print("global rougel, ", global_average_rougel)
+    print("global bertsc, ", global_average_bertsc)
+    print("global semval, ", global_average_semval)
+
+    return global_average_meteor, global_average_rougel, global_average_bertsc, global_average_semval
